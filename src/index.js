7940f95ffeeff3b5d4d5833d7215d59093164bc2
Ali <alimertiskender@gmail.com>
2024-01-29 11:12:11 +0300

    Refactor index.js file

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 require("dotenv/config");
 require("colors");
-const { GiveawaysManager } = require("discord-giveaways");
 const {
   Client,
   GatewayIntentBits,
-  PermissionFlagsBits,
+  PermissionFlagsBits
 } = require("discord.js");
 const eventHandler = require("./handlers/eventHandler");
-const fs = require("fs");
-const { joinVoiceChannel } = require("@discordjs/voice");
+const { GiveawaysManager } = require("discord-giveaways");
 const voiceChannelSchema = require("./schemas/voiceChannelSchema");
 const channelTimeoutSchema = require("./schemas/channelTimeoutSchema");
 const voiceChannelOwnerSchema = require("./schemas/voiceChannelOwnerSchema");
@@ -33,53 +31,6 @@ client.giveawaysManager = new GiveawaysManager(client, {
     embedColor: "#FF0000",
     reaction: "🎉",
   },
-});
-
-function cleanupGiveaways(client) {
-  console.log("Cleaning up giveaways...".random);
-  const activeGiveaways = client.giveawaysManager.giveaways
-    .filter((g) => !g.ended)
-    .map((g) => ({
-      messageID: g.messageID,
-      channelID: g.channelID,
-      guildID: g.guildID,
-      startAt: g.startAt,
-      endAt: g.endAt,
-      ended: g.ended,
-      winnerCount: g.winnerCount,
-      prize: g.prize,
-      messages: g.messages,
-      hostedBy: g.hostedBy,
-      pause: g.pause,
-      setEndTimestamp: g.setEndTimestamp,
-      botsCanWin: g.botsCanWin,
-      exemptPermissions: g.exemptPermissions,
-      bonusEntries: g.bonusEntries,
-      extraData: g.extraData,
-      lastChance: g.lastChance,
-      embedColor: g.embedColor,
-      embedColorEnd: g.embedColorEnd,
-      reaction: g.reaction,
-    }));
-  console.log(`Found ${activeGiveaways.length} active giveaways.`.green);
-  fs.writeFileSync(
-    "./giveaways.json",
-    JSON.stringify(activeGiveaways, null, 2)
-  );
-  console.log("Ended Giveaways cleaned up!".rainbow);
-}
-
-client.once("ready", () => {
-  joinVoiceChannel({
-    channelId: "1200133117533491280",
-    guildId: "882383171323314186",
-    adapterCreator:
-      client.guilds.cache.get("882383171323314186").voiceAdapterCreator,
-  });
-  setTimeout(() => {
-    cleanupGiveaways(client);
-    setInterval(() => cleanupGiveaways(client), 60 * 60 * 1000);
-  }, 5000);
 });
 
 client.on("voiceStateUpdate", async (oldState, newState) => {
@@ -170,6 +121,7 @@ setInterval(async () => {
     }
   });
 }, 60000);
+
 eventHandler(client);
 
 client.login(process.env.TOKEN);
