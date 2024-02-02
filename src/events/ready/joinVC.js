9ac367bba339df60db89ca326a1ff3410ad7bc76
Ali <alimertiskender@gmail.com>
2024-02-02 14:37:08 +0300

    Update package.json with Node.js and npm versions

--- src/events/ready/joinVC.js
+++
@@ -1,9 +0,0 @@
-const { joinVoiceChannel } = require("@discordjs/voice");
-module.exports = async (client) => {
-  joinVoiceChannel({
-    channelId: "1200133117533491280",
-    guildId: "882383171323314186",
-    adapterCreator:
-      client.guilds.cache.get("882383171323314186").voiceAdapterCreator,
-  });
-};
